51febf3ea810165018e7081ad7e1b34254aea219
TheF0x0 <kitsune-alex@gmx.de>
2023-04-06 23:32:50 +0200

    Implement speed update logic

--- js/index.js
+++ js/index.js
@@ -5,26 +5,91 @@
 
 setInterval(update_page, 250)
 
+const btn_power_on = document.getElementById('btn-power-on')
+const btn_power_off = document.getElementById('btn-power-off')
+const speed_slider = document.getElementById('speed')
+
 const get_address = () => document.getElementById('address').value
 const get_password = () => document.getElementById('password').value
+const get_speed = () => speed_slider.value
 const make_address = (endpoint) => `https://${get_address()}/${endpoint}`
 
+let power_state = false
+let previous_power_state = false
+
 async function make_request(method, endpoint, data) {
-    return fetch(make_address(endpoint), {method: method, ...data})
-        .then(res => res.json())
-        .catch(err => console.error(`Could not fulfill request: ${err}`))
+    return fetch(make_address(endpoint), {
+        method: method,
+        ...data
+    }).then(res => res.json())
 }
 
-function update_state() {
-    make_request('GET', 'getstate').then(state => {
-        document.getElementById('power-state').innerHTML = state['is_on']
-            ? '<b class="power-state-active">Device is active</b>'
-            : '<b class="power-state-idle">Device is idle</b>'
-        document.getElementById('target-speed').innerText = `Target Speed: ${state['target_speed']}`
-        document.getElementById('actual-speed').innerText = `Actual Speed: ${state['actual_speed']}`
-    })
+btn_power_on.onclick = () => {
+    btn_power_on.disabled = true
+
+    make_request('POST', 'enqueue', {
+        body: JSON.stringify({
+            password: get_password(),
+            tasks: [
+                {
+                    type: 0,
+                    is_on: true
+                }
+            ]
+        })
+    }).catch(err => console.log(err))
+}
+
+btn_power_off.onclick = () => {
+    btn_power_off.disabled = true
+
+    make_request('POST', 'enqueue', {
+        body: JSON.stringify({
+            password: get_password(),
+            tasks: [
+                {
+                    type: 0,
+                    is_on: false
+                }
+            ]
+        })
+    }).catch(err => console.log(err))
+}
+
+speed_slider.onmouseup = () => {
+    speed_slider.disabled = true
+
+    make_request('POST', 'enqueue', {
+        body: JSON.stringify({
+            password: get_password(),
+            tasks: [
+                {
+                    type: 1,
+                    speed: parseInt(get_speed())
+                }
+            ]
+        })
+    }).catch(err => console.log(err))
 }
 
 function update_page() {
-    update_state()
+    make_request('GET', 'getstate').then(state => {
+        previous_power_state = power_state
+        power_state = state['is_on']
+
+        if (previous_power_state !== power_state) {
+            speed_slider.value = power_state ? 1 : 0
+        }
+
+        btn_power_on.disabled = power_state
+        btn_power_off.disabled = !power_state;
+        speed_slider.disabled = !power_state;
+
+        document.getElementById('power-state').innerHTML = power_state
+            ? '<b class="power-state-active">Device is active</b>'
+            : '<b class="power-state-idle">Device is idle</b>'
+
+        document.getElementById('target-speed').innerText = `Target Speed: ${state['target_speed']}`
+        document.getElementById('actual-speed').innerText = `Actual Speed: ${state['actual_speed']}`
+    }).catch(err => console.error(err))
 }
